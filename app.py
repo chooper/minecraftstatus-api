2de84dc5feb0826149edd90a5ebdd10ccfb12593
Charles Hooper <charles@heroku.com>
2015-05-09 18:03:11 -0700

    add an api

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python
 
+import os
+from flask import Flask,jsonify
 from mcstatus import MinecraftServer
 
-# If you know the host and port, you may skip this and use MinecraftServer("example.org", 1234)
-server = MinecraftServer.lookup("charlotte.pearachute.net:25565")
+app = Flask(__name__)
 
-# 'status' is supported by all Minecraft servers that are version 1.7 or higher.
-status = server.status()
-print("The server has {0} players and replied in {1} ms".format(status.players.online, status.latency))
+@app.route("/playersonline/<server>", methods=['GET'])
+def playersonline(server):
+    server = MinecraftServer.lookup(server)
+    query = server.query()
+    return jsonify(players=list(query.players.names))
 
-# 'ping' is supported by all Minecraft servers that are version 1.7 or higher.
-# It is included in a 'status' call, but is exposed separate if you do not require the additional info.
-latency = server.ping()
-print("The server replied in {0} ms".format(latency))
 
-# 'query' has to be enabled in a servers' server.properties file.
-# It may give more information than a ping, such as a full player list or mod information.
-query = server.query()
-print("The server has the following players online: {0}".format(", ".join(query.players.names)))
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', '5000'))
+    app.run(host='0.0.0.0', port=port)
 
